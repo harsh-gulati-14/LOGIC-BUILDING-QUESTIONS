fcbe484bca059d8f3a8b5b5790455dcff6682057
HARSH GULATI <58563505+harsh-gulati-14@users.noreply.github.com>
2019-12-17 12:21:58 +0530

    Add files via upload

---
+++ Addition and Subtraction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a;
+	cin>>b;
+	cout<<"sum is "<<a+b<<endl;
+	cout<<"difference is"<<a-b<<endl;
+	return 0;
+}
