b100c03ca3ff601ebc4920efc62dd0beb1f6776d
HARSH GULATI <harshgulati1409@gmail.com>
2019-12-17 12:15:59 +0530

    INPUT AND OUTPUT OF sTRING

---
+++ TAKING INPUT AND PRINTING STRINGS.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	char s[30];
+	cin>>s; //or you can also use gets(s); 
+	cout<<s;//or you can also use puts(s);
+	return 0;
+}
